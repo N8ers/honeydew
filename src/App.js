e33c988b9e2a90886a4be51b12e7d6d52e48d698
sheryak_n <nathanraysheryak@gmail.com>
2022-06-12 14:09:13 -0400

    implement basic redux counter example

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import * as React from "react";
 import styles from "./App.module.css";
 
 import TodoContainer from "./components/TodoContainer.js";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div>
       <h1 className={styles.title}>honeydew</h1>
+      <Counter />
       <TodoContainer title="grocery list" />
     </div>
   );
---
+++ src/store/store.js
@@ -0,0 +1,38 @@
+import { createStore, combineReducers } from "redux";
+
+import { countReducer } from "./countReducer";
+
+export const initialState = {
+  // taskLists: [
+  //   { id: 1, title: "Grocery List" },
+  //   { id: 2, title: "Tricks to teach Tsuki" },
+  //   { id: 3, title: "Weekend Chores" },
+  // ],
+  // selectedTaskList: {
+  //   id: 1,
+  //   title: "Grocery List",
+  //   tasks: [
+  //     { id: 1, complete: false, title: "throw rocks" },
+  //     { id: 2, complete: false, title: "eat socks" },
+  //     { id: 3, complete: false, title: "shake blocks" },
+  //   ],
+  //   invitedFriends: [{ id: 1, username: "Goon" }],
+  // },
+  // user: {
+  //   username: "N8ers",
+  //   email: "____",
+  // },
+  // friends: [
+  //   { id: 1, username: "Goon" },
+  //   { id: 2, username: "Tsuki" },
+  // ],
+  counter: {
+    count: 11,
+  },
+};
+
+const reducer = combineReducers({
+  countReducer,
+});
+
+export const store = createStore(reducer);
