a54ce72e49580e9e9c08b71ffd5ba825ad31e7d6
sheryak_n <nathanraysheryak@gmail.com>
2022-06-21 07:49:51 -0400

    run lint on App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-import styles from "./App.module.css";
+import ListContainer from "./components/pages/List/ListContainer/ListContainer.js"
+import Counter from "./components/pages/Counter/Counter/Counter"
+import ListsContainer from "./components/pages/Lists/ListsContainer/ListsContainer"
+import FriendsContainer from "./components/pages/Friends/FriendsContainer/FriendsContainer"
 
-import ListContainer from "./components/pages/List/ListContainer/ListContainer.js";
-import Counter from "./components/pages/Counter/Counter/Counter";
-import ListsContainer from "./components/pages/Lists/ListsContainer/ListsContainer";
-import FriendsContainer from "./components/pages/Friends/FriendsContainer/FriendsContainer";
-
-import Main from "./layouts/Main";
-import Auth from "./layouts/Auth";
-import PageNotFound from "./layouts/PageNotFound";
+import Main from "./layouts/Main"
+import Auth from "./layouts/Auth"
+import PageNotFound from "./layouts/PageNotFound"
 
 function App() {
   return (
     <BrowserRouter>
-      <div className={styles.links}></div>
       <Routes>
         <Route path="/" element={<Main />}>
           <Route path="/lists" element={<ListsContainer />} />
@@ -30,7 +28,7 @@ function App() {
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
