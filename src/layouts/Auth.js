1803db839be5e514fd971eb9fba360d538ba51ce
sheryak_n <nathanraysheryak@gmail.com>
2022-06-25 06:32:30 -0400

    make basic login form

--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -1,14 +1,41 @@
 import { Link } from "react-router-dom"
-import { Box, Button, Card } from "@mui/material"
+import { Box, Button, Card, TextField } from "@mui/material"
 
 function Auth() {
+  const login = (event) => {
+    event.preventDefault()
+    alert("loggin in!")
+  }
+
   return (
     <Box>
-      <Card variant="outlined" sx={{ width: 500, margin: "0 auto" }}>
+      <Card
+        variant="outlined"
+        sx={{ width: 500, margin: "50px auto", padding: "50px" }}
+      >
         <h1>I'm Auth</h1>
         <p>Pretty much the landing page if you're not signed in, i guess.</p>
+
+        <form onSubmit={login}>
+          <div>
+            <TextField id="standard-basic" label="email" variant="standard" />
+          </div>
+          <div>
+            <TextField
+              id="standard-basic"
+              label="password"
+              variant="standard"
+            />
+          </div>
+          <Button type="submit" variant="contained">
+            Sign in
+          </Button>
+        </form>
+
+        <br />
+        <br />
         <Link to={"/lists"}>
-          <Button variant="contained">Log In</Button>
+          <Button>Go Home</Button>
         </Link>
       </Card>
     </Box>
