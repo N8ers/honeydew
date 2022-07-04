41687e4f3b7ee469ae0efb968f9f2e94ba2296da
sheryak_n <nathanraysheryak@gmail.com>
2022-07-04 17:52:44 -0400

    get dropdown semi-working

--- src/components/pages/List/Collaborators/Collaborators.js
+++ src/components/pages/List/Collaborators/Collaborators.js
@@ -12,63 +12,80 @@ function Collaborators(props) {
 
   const friendDataFromState = useSelector((state) => state.friends)
 
-  const [invitedFriends, setInvitedFriends] = useState([])
   const [modal, setModal] = useState(false)
+  const [value, setValue] = useState(invitedFriendsDataFromState)
 
-  const handleGetListById = async () => {
-    await dispatch(getFriends())
+  const handleGetListById = () => {
+    dispatch(getFriends())
   }
 
   useEffect(() => {
     handleGetListById()
-    setInvitedFriends(invitedFriendsDataFromState)
   }, [])
 
-  const handleInvitedCollaboratorChange = (value) => {
-    console.log("event ", value.target.value)
-    setModal(true)
-  }
+  useEffect(() => {
+    setValue(invitedFriendsDataFromState)
+  }, [invitedFriendsDataFromState])
 
   return (
     <div>
       <div>
         <h4>Collaborators</h4>
         <ul>
-          {invitedFriends.map((friend) => (
+          {invitedFriendsDataFromState.map((friend) => (
             <li key={friend.id}>{friend.username}</li>
           ))}
         </ul>
       </div>
 
-      <Autocomplete
-        multiple
-        id="tags-standard"
-        options={friendDataFromState}
-        getOptionLabel={(option) => option.username}
-        defaultValue={[]}
-        onChange={handleInvitedCollaboratorChange}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            variant="standard"
-            label="Multiple values"
-            placeholder="Favorites"
-          />
-        )}
-      />
+      <Button onClick={() => setModal(true)}>manage collaborators</Button>
 
       <Backdrop open={modal}>
-        <Card variant="outlined" sx={{ width: 500, margin: "0 auto" }}>
-          <h1>hi</h1>
-          <Button onClick={() => setModal(false)}>close</Button>
+        <Card variant="outlined" sx={{ width: 500, padding: 5 }}>
+          <div>
+            <h4>Collaborators</h4>
+            <ul>
+              {invitedFriendsDataFromState.map((friend) => (
+                <li key={friend.id}>{friend.username}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <div>'value' state: {JSON.stringify(value)}</div>
+            <br />
+            <div>'options': {JSON.stringify(friendDataFromState)}</div>
+          </div>
+          <br />
+          <br />
+
+          <Autocomplete
+            multiple
+            options={friendDataFromState}
+            filterSelectedOptions
+            getOptionLabel={(option) => option.username}
+            value={value}
+            defaultValue={[invitedFriendsDataFromState]}
+            onChange={(_, newValue) => setValue(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="standard"
+                label="Multiple values"
+                placeholder="Favorites"
+              />
+            )}
+          />
+
+          <br />
+          <br />
+          <Button onClick={() => setModal(false)}>save</Button>
+          <Button onClick={() => setModal(false)}>cancel</Button>
+          <Link to={"/friends"}>
+            <Button>Invite a new collaborator!</Button>
+          </Link>
         </Card>
       </Backdrop>
-
-      <hr />
-
-      <Link to={"/friends"}>
-        <Button>Invite a new collaborator!</Button>
-      </Link>
     </div>
   )
 }
