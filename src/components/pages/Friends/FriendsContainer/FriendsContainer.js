cb71ac017c1dfe0cfddf1716294b5761a3a2db63
sheryak_n <nathanraysheryak@gmail.com>
2022-07-08 07:32:21 -0400

    work on invite friend modal

--- src/components/pages/Friends/FriendsContainer/FriendsContainer.js
+++ src/components/pages/Friends/FriendsContainer/FriendsContainer.js
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
-import { Button, Backdrop, Card } from "@mui/material"
+import {
+  Button,
+  Backdrop,
+  Card,
+  TextField,
+  Alert,
+  IconButton,
+} from "@mui/material"
+import { Close } from "@mui/icons-material"
 
 import FriendCard from "../FriendCard/FriendCard"
 import { getFriends } from "../../../../store/actions"
@@ -8,6 +16,7 @@ import { getFriends } from "../../../../store/actions"
 function FriendsContainer() {
   const [friends, setFriends] = useState([])
   const [isOpen, setIsOpen] = useState(false)
+  const [invitationIsSent, setInvitationIsSent] = useState(false)
 
   const friendDataFromState = useSelector((state) => state.friends)
 
@@ -26,12 +35,50 @@ function FriendsContainer() {
     <div>
       <h1>Friends</h1>
 
-      <Backdrop open={isOpen}>
-        <Card sx={{ backgroundColor: "white", padding: 5 }}>
-          <h1>hello there</h1>
-          <Button variant="contained" onClick={() => setIsOpen(false)}>
-            close
-          </Button>
+      <Backdrop
+        open={isOpen}
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        {/* break me out into my own component */}
+        <Card sx={{ backgroundColor: "white", padding: 5, width: 500 }}>
+          <IconButton
+            aria-label="close"
+            onClick={() => {
+              setInvitationIsSent(false)
+              setIsOpen(false)
+            }}
+            sx={{ float: "right" }}
+          >
+            <Close />
+          </IconButton>
+
+          <h1>Invite a friend!</h1>
+          <p>
+            We care about privacy and won't spam anyone! If they aren't a member
+            of HoneyDew, ask them to join first. :)
+          </p>
+          <div>
+            <div>Find a friend via email: </div>
+            <TextField
+              id="outlined-basic"
+              label="email"
+              variant="outlined"
+              size="small"
+              sx={{ marginTop: 1 }}
+            />
+            <Button
+              variant="contained"
+              onClick={() => setInvitationIsSent(true)}
+              sx={{ marginLeft: 3, marginTop: 1 }}
+            >
+              invite!
+            </Button>
+          </div>
+          {invitationIsSent && (
+            <Alert severity="success" sx={{ marginTop: 2 }}>
+              This is a success alert — check it out!
+            </Alert>
+          )}
         </Card>
       </Backdrop>
 
